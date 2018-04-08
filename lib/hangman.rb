c6d0da179ceea178aa2086af77c20679855a2ec9
Uy Binh <uybinh@gmail.com>
2018-04-08 12:20:26 +0700

    Add win? method to Hangman class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,12 @@
 class Hangman
+  attr_reader :incomplete_word
 
+  def initialize(keyword)
+    @keyword = keyword
+    @incomplete_word = Array.new(keyword.length, "_")
+  end
 
-
+  def win?
+    @keyword == @incomplete_word.join
+  end
 end
